5d94de2af86b2f7bd9905edd49d2f4dbe4b9d6eb
Brian Cooke <brian.cooke@live.co.uk>
2018-05-07 17:21:03 +0100

    Created Kaiju abstract class.

--- src/main/java/kaiju/Kaiju.java
+++ src/main/java/kaiju/Kaiju.java
@@ -1,4 +1,19 @@
 package kaiju;
 
-public class Kaiju {
+public abstract class Kaiju {
+    private String name;
+    private int healthValue;
+    private int attackValue;
+
+    public Kaiju(String name, int healthValue, int attackValue) {
+        this.name = name;
+        this.healthValue = healthValue;
+        this.attackValue = attackValue;
+    }
+
+    public String roar() {
+        return "ROAR!";
+    }
+
+
 }
